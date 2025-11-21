5bdbf7abfef85b00d055e8519564499dd289e123
mahesh4033 <msahu4033@gmail.com>
2025-11-21 22:58:01 +0530

    assignment submit

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+Num1=int(input("Enter the first number: "))
+Num2=int(input("Enter the second number: "))
+addition=Num1+Num2
+subtraction=Num1-Num2
+multiplication=Num1*Num2
+division=Num1/Num2
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)

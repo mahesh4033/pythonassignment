5a85349a5608e6001ac3b098ccf7eb0f3665e9c9
mahesh4033 <msahu4033@gmail.com>
2025-11-22 00:35:09 +0530

    commit changes.

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,12 @@
 Num1=int(input("Enter the first number: "))
 Num2=int(input("Enter the second number: "))
+
 addition=Num1+Num2
+
 subtraction=Num1-Num2
+
 multiplication=Num1*Num2
+
 division=Num1/Num2
 print("Addition:",addition)
 print("Subtraction:",subtraction)
